5c48c2f94d2f48f888320b7b8ff3e423170e7717
Mohamed Elorbany <m.elorbany@ymail.com>
2016-10-05 08:22:51 +0300

    Arabic Support

---
+++ Assets/Game/Scripts/FixArabicText.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using ArabicSupport;
+using UnityEngine.UI;
+
+
+public class FixArabicText : MonoBehaviour {
+
+	public string text;
+	public bool tashkeel = true;
+	public bool hinduNumbers = true;
+
+	// Use this for initialization
+	void Start () {
+		gameObject.GetComponent<Text>().text = ArabicFixer.Fix(text, tashkeel, hinduNumbers);
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
